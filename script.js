7303729c76f8e50209efa967ced9084f58733a76
Nicodemo1996 <nicodemoprincipe2@gmail.com>
2025-09-03 22:20:51 +0200

    [ADD] FRIZZBUZZ

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ for (let i = 1; i <= 100; i++) {
   //condizione per i multipli di 5//
 } else if (i % 5 === 0) {
   console.log("Buzz")
+
+  //condizione per i multipli di 3 e di 5//
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FrizzBuzz")
+
+
+
